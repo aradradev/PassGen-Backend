17ec1f45c664647587e04fd0dd65baf8731c1e9e
Abdoul <abdoulramanediallo44@gmail.com>
2023-11-23 14:16:28 +0000

    adding feature user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :set_user, only: %i[show update destroy]
+  before_action :set_user, only: %i[show update destroy]
 
-    def index
-        @users = User.all
-        render json: @users
-    end
+  def index
+    @users = User.all
+    render json: @users
+  end
 
-    def show
-        render json: @user
-    end
+  def show
+    render json: @user
+  end
 
-    private
-    def set_user
-        @user = User.find(:params[:id])
-    end
+  private
+
+  def set_user
+    @user = User.find(:params[:id])
+  end
 end
