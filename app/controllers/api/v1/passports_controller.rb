2eb608ee30f318a92f33d7352205cffed7205ba9
Abdoul <abdoulramanediallo44@gmail.com>
2023-11-23 16:01:39 +0000

    adding the passport api

---
+++ app/controllers/api/v1/passports_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::PassportsController < ApplicationController
+  before_action :set_user
+  before_action :set_passport, only: %i[show update]
+
+  def index
+    @passports = @user.passports
+    render json: @passports
+  end
+
+  def create
+    @passport = @user.passports.create(passport_params)
+    render json: @passport
+  end
+
+  def show
+    render json: @passport
+  end
+
+  def update
+    @passport.update(passport_params) if params[:passport_details].present?
+    render json: @passport
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_passport
+    @passport = @user.passports.find(params[:id])
+  end
+
+  def passport_params
+    params.permit(:passport_number, :issue_date, :expiration_date, :country)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create, :update, :destroy]
+      resources :users, only: [:index, :show, :create, :update, :destroy] do
+        resources :passports, only: %i[index create show update]
+      end
     end
   end
 end
