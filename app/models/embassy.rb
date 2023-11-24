d93d9c2257dc785710f4b52e906548708411e2e2
Abdoul <abdoulramanediallo44@gmail.com>
2023-11-24 13:05:38 +0000

    adding embassy models

--- app/models/embassy.rb
+++ app/models/embassy.rb
@@ -1,3 +1,12 @@
 class Embassy < ApplicationRecord
-  validates :country, :name, :location, :appointment_date, presence: true
+  has_many :embassy_appointments
+  has_many :users, through: :embassy_appointments
+
+  validates :country, :name, :location, presence: true
+  validates :continent, presence: true
+  validate :appointment_date_should_be_in_the_future
+
+  def appointment_date_should_be_in_the_future
+    errors.add(:appointment_date, 'should be in the future') if appointment_date.present? && appointment_date <= Time.current
+  end
 end
